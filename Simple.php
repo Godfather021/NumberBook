12eff5c4d82882df7982f19ae9c7e3f2ed9f964a
Saleh Bin Homoud <iPain7@gmail.com>
2016-01-16 03:43:45 +0300

    Create Simple.php

---
+++ Simple.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Find numbers and names | Arabs | API PHP .
+ *
+ * @package : NumberBook;
+ * @author : Saleh Bin Homud;
+ * @version : 1.0;
+ * @link : https://github.com/Saleh7;
+ */
+
+$Key    = 'API keys. Here'; // API keys | The key here is https://telegram.me/Pain7
+$Search = 'test'; // Numbers OR Names
+$Cntry  = ''; // 966 or 967 .. | Cntry is not currently enabled
+$Count  = ''; // Control Number of Results | Count is not currently enabled
+
+$uRLApi = 'https://api.igeek.io/v1/'; //
+$Results = @file_get_contents("$uRLApi?key=$Key&str=$Search");
+print_r($Results);
+
+/* Results ..
+status:200 | Success
+status:304 | Empty String
+status:404 | Sorry, no results
+status:400 | The key is not valid
+*/
+
+ ?>
